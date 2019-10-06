478cf1853314a7ff8e9d5cb22920275bb6820345
Shamim Hossain <hossains159@gmail.com>
2019-10-06 12:49:11 +0600

    solve multiple query issue in category index

--- app/Http/Controllers/Author/CommentController.php
+++ app/Http/Controllers/Author/CommentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
@@ -19,13 +20,24 @@ class CommentController extends Controller
     */
     public function index()
     {
-        $posts = Auth::user()->posts;
+        $data = DB::table('users')
+            ->join('comments', 'users.id', '=', 'comments.user_id')
+            ->join('posts', 'comments.post_id', '=', 'posts.id')
+            ->select(
+                'comments.id',
+                'users.name',
+                'users.image AS profile',
+                'comments.created_at AS comment_date',
+                'comments.comment',
+                'posts.slug',
+                'posts.image AS thumbnail',
+                'posts.title',
+                'posts.created_at AS post_date'
+            )
+            ->where('users.id', Auth::user()->id)
+            ->get();
 
-        $count = collect($posts)->map(function ($post) {
-            return $post->comments->count();
-        })->sum();
-
-        return view('author.comments', compact('posts', 'count'));
+        return view('author.comments', compact('data'));
     }
 
     /**
--- resources/views/author/comments.blade.php
+++ resources/views/author/comments.blade.php
@@ -16,7 +16,7 @@ rel="stylesheet">
             <div class="card">
                 <div class="header">
                     <h2>
-                        ALL COMMENTS <span class="badge bg-red">{{ $count }}</span>
+                        ALL COMMENTS <span class="badge bg-red">{{ count($data) }}</span>
                     </h2>
                 </div>
                 <div class="body">
@@ -39,60 +39,57 @@ rel="stylesheet">
                                 </tr>
                             </tfoot>
                             <tbody>
-                                @foreach ($posts as $key=>$post)
-                                    @foreach ($post->comments as $comment)
-                                    <tr>
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <a class="avatar" href="#">
-                                                        <img class="media-object" src="{{ asset('/storage/profiles/'. $comment->user->image) }}" 
-                                                        alt="{{ $comment->user->name }}" width="64" height="64">
-                                                    </a>
-                                                </div>
-                                                <div class="media-body">
-                                                    <h4 class="media-heading">{{ $comment->user->name }}
-                                                    <small>{{ $comment->created_at->diffForHumans() }}</small></h4>
-                                                    <p>{{ $comment->comment }}</p>
-                                                    <a href="{{ route('post.details', $comment->post->slug. "#comments") }}">Reply</a>
-                                                </div>
+                                @foreach ($data as $key=>$item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>
+                                        <div class="media">
+                                            <div class="media-left">
+                                                <a class="avatar" href="#">
+                                                    <img class="media-object" src="{{ asset('/storage/profiles/'. $item->profile) }}" 
+                                                    alt="{{ $item->name }}" width="64" height="64">
+                                                </a>
                                             </div>
-                                        </td>
-                                        <td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <a target="_blank" href="{{ route('post.details', $comment->post->slug) }}">
-                                                        <img class="media-object" src="{{ asset('/storage/posts/'. $comment->post->image) }}" 
-                                                        alt="{{ $comment->post->title }}" height="64">
-                                                    </a>
-                                                </div>
-                                                <div class="media-body">
-                                                    <a target="_blank" href="{{ route('post.details', $comment->post->slug) }}">
-                                                        <h4 class="media-heading">{{ Str::limit($comment->post->title, 40) }}</h4>
-                                                    </a>
-                                                    <p>by <strong>{{ $comment->post->user->name }}</strong></p>
-                                                </div>
+                                            <div class="media-body">
+                                                <h4 class="media-heading">{{ $item->name }}
+                                                <small>{{ \Carbon\Carbon::parse($item->comment_date)->diffForHumans() }}</small></h4>
+                                                <p>{{ $item->comment }}</p>
+                                                <a href="{{ route('post.details', $item->slug. "#comments") }}">Reply</a>
                                             </div>
-                                        </td>
-                                        <td>
-                                            <button 
-                                                class="btn btn-xs btn-danger waves-effect"
-                                                type="button"
-                                                onclick="deleteDialog({{ $comment->id }})"
-                                            >
-                                                <i class="material-icons">delete</i>
-                                            </button>
-                                            <form id="delete-form-{{ $comment->id }}" 
-                                                action="{{ route('author.comment.destroy', $comment->id) }}" 
-                                                method="POST" style="display: none;">
-                                                @csrf
-                                                @method('DELETE')
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                    
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="media">
+                                            <div class="media-left">
+                                                <a target="_blank" href="{{ route('post.details', $item->slug) }}">
+                                                    <img class="media-object" src="{{ asset('/storage/posts/'. $item->thumbnail) }}" 
+                                                    alt="{{ $item->title }}" height="64">
+                                                </a>
+                                            </div>
+                                            <div class="media-body">
+                                                <a target="_blank" href="{{ route('post.details', $item->slug) }}">
+                                                    <h4 class="media-heading">{{ Str::limit($item->title, 40) }}</h4>
+                                                </a>
+                                                <p>at <strong>{{ \Carbon\Carbon::parse($item->post_date)->diffForHumans() }}</strong></p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <button 
+                                            class="btn btn-xs btn-danger waves-effect"
+                                            type="button"
+                                            onclick="deleteDialog({{ $item->id }})"
+                                        >
+                                            <i class="material-icons">delete</i>
+                                        </button>
+                                        <form id="delete-form-{{ $item->id }}" 
+                                            action="{{ route('author.comment.destroy', $item->id) }}" 
+                                            method="POST" style="display: none;">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
