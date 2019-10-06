a081293223e17f6d623231002119fe247bd48f85
Shamim Hossain <hossains159@gmail.com>
2019-10-06 15:38:36 +0600

    fix loading issue in admin comment section

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -6,6 +6,7 @@ use App\Comment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
@@ -18,9 +19,24 @@ class CommentController extends Controller
     */
     public function index()
     {
-        $comments = Comment::latest()->get();
+        $data = DB::table('users')
+            ->join('comments', 'users.id', '=', 'comments.user_id')
+            ->join('posts', 'comments.post_id', '=', 'posts.id')
+            ->select(
+                'comments.id',
+                'users.name',
+                'users.image AS profile',
+                'comments.created_at AS comment_date',
+                'comments.comment',
+                'posts.slug',
+                'posts.image AS thumbnail',
+                'posts.title',
+                'posts.created_at AS post_date'
+            )
+            ->orderBy('comments.id', 'desc')
+            ->get();
 
-        return view('admin/comments', compact('comments'));
+        return view('admin.comments', compact('data'));
     }
 
     /**
